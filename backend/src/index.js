1499b169da6ec8601e9297dedd0f9f3816b89a2f
Vitor Ribeiro <vitorribeirovrm@outlook.com>
2021-08-06 14:04:35 -0300

    configuracoes inicias, adicionando o express e o nodemon

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+const express = require('express'); //Importamos
+
+const app = express(); // instanciamos o express
+
+app.get('/projects', (request, response) => {  //requisição feita para o cliente se comunicar com a API
+    return response.json({
+        menssage: 'Hello Dev!'
+    });
+});
+
+app.listen(3333, () => {
+    console.log('Backend started')
+}); //definimos a porta que o express vai trabalhar do localhost
